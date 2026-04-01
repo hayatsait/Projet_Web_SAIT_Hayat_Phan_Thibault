0752a86ca14306106fd214bc06a8aa41b381d38e
hayatsait <saithayat4@gmail.com>
2026-04-01 14:19:04 +0200

    Initialisation su serveur et implémentation des routes de la page d'acceuil et de la page de connexion

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('acceuil.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/annonces')
+def annonces():
+    return "Page annonces"
+
+@app.route('/nouvelle_annonce')
+def nouvelle_annonce():
+    return "Nouvelle annonce"
+
+@app.route('/register')
+def register():
+    return "Register"
+
+if __name__ == '__main__':
+    app.run(debug=True)
